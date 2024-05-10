23132640d14caf676de20d03feb91bdd8a21c4b7
letterly <70981501+letterly@users.noreply.github.com>
2024-05-10 10:50:19 -0400

    Add files via upload

---
+++ wazhazhe.js
@@ -0,0 +1,159 @@
+lessons = {
+    "Orthography I": {
+        grammar: "The Osage language uses its own alphabet which is completely separate from the Latin alphabet used for English. However, many of the letters are reminiscient of ones found in the English alphabet, making it very easy to learn. For example, 𐓒 looks like the letter Z and makes the same sound. Another example is how 𐒵 looks like a combination of C and h and makes the 'Ch' sound.",
+        vocab: [
+            "𐒰: 'a' as in 'all'",
+            "𐓀: 'm' as in 'mall'",
+            "𐓁: 'n' as in 'no'",
+            "𐓆: 's' as in 'sand'",
+        ],
+        questions: [
+            "Which letter makes the 'm' sound? | 𐓀;𐓁;𐓐",
+            "Which letter is not a vowel? | 𐓄;𐒷;𐒰",
+            "𐒹 is equivalent to what English letter? | h;w;l",
+            "𐓆 is equivalent to what English letter? | s;l;e",
+            "This letter is pronounced like 'sh' in 'shark': | 𐓇;𐒵;𐒴",
+        ],
+    },
+    "Orthography II": {
+        grammar: "Not every letter has an equivalent in the English language; hence the need for a new Osage script. Herbert 'Mogri' Mongrain invented the alphabet around 2006 and it was modified over the next decade and a half.",
+        vocab: [
+            "𐓍: 'th' as in 'this'",
+            "𐒾: like the 'ky' sound found in 'cube'",
+            "𐒴: 'br' as in 'bring'",
+            "𐓃: 'oin' as in 'ointment'",
+        ],
+        questions: [
+            "This letter is in between English 'd' and 't' | 𐓈;𐓄;𐓍",
+            "Which letter is not a diphthong (two vowels next to each other) | 𐒰͘;𐓃;𐒱",
+            "This letter also has a pre-aspirated AND post-aspirated version | 𐓊;𐓈;𐓄",
+            "This vowel has a nasal form | 𐒰;𐒷;𐓎",
+            "This letter is pronounced like 'th' in 'though': | 𐓍;𐓉;𐓏",
+        ],
+    },
+    "Colors": {
+        grammar: "Adjectives in Osage kinda don't exist, they are actually stative verbs. A stative verb is a verb that describes a characteristic of something, for example 'John stinks' describes that John smells bad, it doesn't refer to an action John is taking.",
+        vocab: [
+            "𐓒𐒻́: yellow",
+            "𐓓𐒻́𐒹𐒻: brown",
+            "𐓓𐓎̋𐓊𐒷: red",
+            "𐓆𐒰́𐓄𐒷: black",
+            "𐓓𐓎̋𐓊𐒷 𐓆𐒰́𐓄𐒷: purple",
+        ],
+        questions: [
+            "Translate the following: 𐓓𐓎̋𐓊𐒷 𐓆𐒰́𐓄𐒷 | purple",
+            "Which color is not in the rainbow? | 𐓓𐒻́𐒹𐒻;𐓓𐒻́;𐓓𐓎̋𐓊𐒷",
+            "Find the missing letter in the word for 'black': 𐓆𐒰́𐓄𐒷 | 𐒷;𐒻;𐓎",
+        ],
+    },
+    "Fruits": {
+        grammar: "",
+        vocab: [
+            "𐓍𐒰𐓌𐒷́: to eat",
+            "𐓆𐒰́𐒽𐓎: watermelon",
+            "𐒻̄͘𐓇𐓈𐒰́𐓐𐒻͘ 𐓆𐒰́𐒽𐓎: canteloupe (literally white man's watermelon)",
+            "𐒼𐓂͘𐓄𐒷́: hackberry [a purple berry growing in the Midwest]",
+        ],
+        questions: [
+            "Translate the following: 𐒴𐒰́𐓌𐒷 | I eat",
+            "Translate the following: 𐓆𐓈𐒰́𐓌𐒷 | You eat",
+            "Which fruit does not grow on a tree? | 𐓆𐒰́𐒽𐓎;𐓉𐓂́𐓓𐒰͘𐒼𐒷 𐒷̄𐒼𐓂́͘;𐒼𐓂͘𐓄𐒷́",
+            "Which is a synonym of 𐒰́𐓏𐒰𐓁𐓂͘𐒴𐒷? | 𐒴𐒰́𐓌𐒷;𐓆𐓈𐒰́𐓌𐒷;𐓏𐒰𐓁𐓂́͘𐒴𐒰𐓄𐒷",
+            "Find the missing letter in the word for 'blackberry': 𐓏𐒰_𐒼𐓎́𐒷 𐓆𐒰́𐓄𐒷 | 𐓆;𐓈;𐓄",
+        ],
+    },
+    "Numbers I": {
+        grammar: "Cardinal and ordinal numbers are the same starting at 9. This means the word for 'thirteen' and 'thirteenth' are the same in Osage; though small numbers have separate words like 'five' and 'fifth' ( and )",
+        vocab: [
+            "𐓅𐒷̋𐓂͘𐓄𐒰: seven",
+            "𐓇𐒰́𐓅𐒷: six",
+            "𐓏𐒷𐓇𐒰́𐓅𐒷: sixth",
+            "𐒽𐒻𐒷𐓈𐓂̋𐓄𐒰: eight",
+        ],
+        questions: [
+            "Write '8' in Osage | 𐒽𐒻𐒷𐓈𐓂̋𐓄𐒰",
+            "Write 'sixth' in Osage | 𐓏𐒷𐓇𐒰́𐓅𐒷",
+            "Write '𐓅𐒷̋𐓂͘𐓄𐒰' in digits | 7",
+        ],
+    },
+    "Numbers II": {
+        grammar: "Like English, Osage uses a base-10 numeral system, meaning counting is based on multiples of ten. For example, the word for twenty is actually 'ten two'.",
+        vocab: [
+            "𐒿𐒷́𐒴𐒰͘ 𐒹𐓎̄𐓓𐒻́͘: hundred",
+            "𐒿𐒷́𐒴𐒰͘ 𐓍𐓂̄͘𐓄𐒰́: twenty",
+            "𐒿𐒷́𐒴𐒰͘ 𐓍𐒰̋𐒴𐒻͘: thirty",
+            "𐒿𐒷́𐒴𐒰͘: ten",
+        ],
+        questions: [
+            "Write this in digits: 𐒿𐒷́𐒴𐒰͘ 𐓍𐓂̄͘𐓄𐒰́ 𐒷́𐓊𐒻 𐓏𐒻́͘𐓐𐓊𐒻 | 21",
+            "Write 'hundredth' in Osage | 𐒿𐒷́𐒴𐒰͘ 𐒹𐓎̄𐓓𐒻́͘",
+            "Write 𐒿𐒷́𐒴𐒰͘ 𐓅𐒷́𐓍𐓂̄͘𐓄𐒰 in English | seventy",
+        ],
+    },
+}
+
+
+
+questionNumber = 0
+lesson = ""
+
+
+function vocab(le){
+    lesson = le
+    start.style.display = "none"
+    button.style.display = "block"
+    vocabulary.style.display = "block"
+    vocabulary.innerHTML = lessons[lesson].vocab.map(x => `<tr><td>${x.split(": ")[0]}</td><td>${x.split(": ")[1]}</td></tr>`).join("")
+}
+
+function read(le){
+    lesson = le
+    start.style.display = "none"
+    button.style.display = "block"
+    grammar.style.display = "block"
+    grammar.textContent = lessons[lesson].grammar
+}
+function generateQuestion(){
+    myquestion = lessons[lesson].questions[questionNumber]
+    console.log(myquestion)
+    if(myquestion.startsWith("Write") || myquestion.startsWith("Translate")){
+        alert('h')
+        question.textContent = myquestion.split(" | ")[0]
+        write.style.display = "block"
+        click.style.display = "none"
+    }
+    else{
+        alert('hhhh')
+        question.textContent = myquestion.split(" | ")[0]
+        click.style.display = "block"
+        write.style.display = "none"
+        answers = myquestion.split(" | ")[1].split(";")
+        b0.textContent = answers[0]
+        b1.textContent = answers[1]
+        b2.textContent = answers[2]
+    }
+}
+
+function next(){
+    questionNumber++
+    if(questionNumber < lessons[lesson].questions.length){
+        generateQuestion()
+    }
+    else menu()
+}
+
+function zone(le){
+    lesson = le
+    start.style.display = "none"
+    button.style.display = "block"
+    quiz.style.display = "block"
+    questionNumber = 0
+    generateQuestion()
+}
+function menu(){
+    vocabulary.style.display = "none"
+    grammar.style.display = "none"
+    quiz.style.display = "none"
+    start.style.display = "block"
+    button.style.display = "none"
+}
